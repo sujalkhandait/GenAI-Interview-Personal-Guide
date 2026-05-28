ebb4136ad95e006320561dfdeb10378a7ef8b46d
sujal khandait <sujalkhandait21@gmail.com>
2026-05-28 22:45:26 +0530

    xyz

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ async function startServer() {
     try {
 
         await connectToDB();
-
+        app.set("trust proxy", 1);
         app.listen(PORT, () => {
 
             console.log(`Server is running on port ${PORT}`);
